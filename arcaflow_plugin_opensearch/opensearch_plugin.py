f135e5d91cdb61b369d6ed831dac6c5224867294
Dustin Black <dblack@redhat.com>
2023-12-15 12:11:08 +0100

    ensure lists are converted to Opensearch-compatible formats
    add tests for conversion functions
    
    linting
    
    pass through dicts in list conversion function

--- arcaflow_plugin_opensearch/opensearch_plugin.py
+++ arcaflow_plugin_opensearch/opensearch_plugin.py
@@ -9,6 +9,49 @@ from arcaflow_plugin_sdk import plugin
 from opensearch_schema import ErrorOutput, SuccessOutput, StoreDocumentRequest
 
 
+def convert_to_supported_type(value) -> typing.Dict:
+    type_of_val = type(value)
+    if type_of_val == list:
+        new_list = []
+        for i in value:
+            new_list.append(convert_to_supported_type(i))
+        # A list needs to be of a consistent type or it will
+        # not be indexible into a system like Opensearch
+        return convert_to_homogenous_list(new_list)
+    elif type_of_val == dict:
+        result = {}
+        for k in value:
+            result[convert_to_supported_type(k)] = convert_to_supported_type(value[k])
+        return result
+    elif type_of_val in (float, int, str, bool):
+        return value
+    elif isinstance(type_of_val, type(None)):
+        return str("")
+    else:
+        print("Unknown type", type_of_val, "with val", str(value))
+        return str(value)
+
+
+def convert_to_homogenous_list(input_list: list):
+    # To make all types in list homogeneous, we upconvert them
+    # to the least commom type.
+    # int -> float -> str
+    # bool + None -> str
+    list_type = str()
+    for j in input_list:
+        if type(j) is dict:
+            list_type = dict()
+            break
+        if type(j) in (str, bool, type(None)):
+            list_type = str()
+            break
+        elif type(j) is float:
+            list_type = float()
+        elif type(j) is int and type(list_type) is not float:
+            list_type = int()
+    return list(map(type(list_type), input_list))
+
+
 @plugin.step(
     id="opensearch",
     name="OpenSearch",
@@ -18,6 +61,8 @@ from opensearch_schema import ErrorOutput, SuccessOutput, StoreDocumentRequest
 def store(
     params: StoreDocumentRequest,
 ) -> typing.Tuple[str, typing.Union[SuccessOutput, ErrorOutput]]:
+    document = convert_to_supported_type(params.data)
+    
     try:
         if params.username:
             opensearch = OpenSearch(
@@ -31,7 +76,10 @@ def store(
                 hosts=params.url,
                 verify_certs=params.tls_verify,
             )
-        resp = opensearch.index(index=params.index, body=params.data)
+        resp = opensearch.index(
+            index=params.index,
+            body=document,
+        )
         if resp["result"] != "created":
             raise Exception(f"Document status: {resp['_shards']}")
 
--- tests/unit/test_arcaflow_plugin_opensearch.py
+++ tests/unit/test_arcaflow_plugin_opensearch.py
@@ -34,6 +34,44 @@ class StoreTest(unittest.TestCase):
             )
         )
 
+    def test_convert_to_homogeneous_list(self):
+        test_cases = [
+            ["a", "b", "c"],  # all str
+            ["a", "b", 1],  # One final int to convert to str
+            [1.0, 1, "1"],  # str at end, so upconvert all to str
+            ["1", 1, 1.0],
+            ["1", 1, 1],
+            [1, 1, "1"],
+            [1, 1, 1],
+            [1.0, 1, 1],
+            [1, 1, 1.0],
+        ]
+        # Ensure they're all homogeneous
+        for test_case in test_cases:
+            validate_list_items_homogeous_type(
+                self, opensearch_plugin.convert_to_homogenous_list(test_case)
+            )
+        # Ensure the type matches
+        self.assertEqual(
+            int, type(opensearch_plugin.convert_to_homogenous_list([1, 1, 1])[0])
+        )
+        self.assertEqual(
+            float,
+            type(opensearch_plugin.convert_to_homogenous_list([1, 1, 1.0])[0]),
+        )
+        self.assertEqual(
+            str,
+            type(opensearch_plugin.convert_to_homogenous_list([1, 1.0, "1.0"])[0]),
+        )
+
+
+def validate_list_items_homogeous_type(t, input_list):
+    if len(input_list) == 0:
+        return  # no problem with an empty list
+    expected_type = type(input_list[0])
+    for item in input_list:
+        t.assertEqual(type(item), expected_type)
+
 
 if __name__ == "__main__":
     unittest.main()
